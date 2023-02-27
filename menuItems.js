919718c714804121b81514ad274ce0a034f6ea27
Malek Alarmash <81653238+Malekalarmash@users.noreply.github.com>
2023-02-27 10:31:14 -0500

    intital

---
+++ menuItems.js
@@ -0,0 +1,38 @@
+let menuItems = [
+          {
+            id: 1,
+            name: "Hamburger",
+            price: 10.99,
+            fulfilled: false,
+            cancelled: false
+          },
+          {
+            id: 2,
+            name: "Cheese Pizza",
+            price: 14.99,
+            fulfilled: false,
+            cancelled: false
+          },
+          {
+            id: 3,
+            name: "Caesar Salad",
+            price: 8.99,
+            fulfilled: false,
+            cancelled: false
+          },
+          {
+            id: 4,
+            name: "Fish and Chips",
+            price: 12.99,
+            fulfilled: false,
+            cancelled: false
+          },
+          {
+            id: 5,
+            name: "Veggie Burger",
+            price: 9.99,
+            fulfilled: false,
+            cancelled: false
+          }
+        ];
+module.exports = menuItems
